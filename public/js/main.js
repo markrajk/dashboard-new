22a547e9587f53d367c8e9cbb528b3cc09a6a6a6
markrajk <marko88rajkovic@gmail.com>
2020-10-07 13:44:42 +0200

    side menu closed on tablet

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,20 @@
-var sidenav = document.querySelector('.sidebar');
-var sidenavMenu = document.querySelector('.sidebar-menu');
-var sidenavMenuToggle = document.querySelector('.sidebar-menu-toggle');
-var sidenavMenuToggleIcon = document.querySelector('.sidebar-menu-toggle i');
+let sideMenu = document.querySelector(".side-menu");
+let sideMenuTrigger = document.querySelector(".toggle-side-menu");
 
-sidenavMenuToggle.addEventListener('click', function () {
-  sidenav.classList.toggle('sidebar-menu-open-anim');
-  sidenavMenuToggleIcon.classList.toggle('sidebar-menu-close-icon-rotate');
-});
-
-function closeMenuOnSmall() {
-  if (window.innerWidth > 1320) {
-    sidenav.classList.add('sidebar-menu-open-anim');
-    sidenavMenuToggleIcon.classList.add('sidebar-menu-close-icon-rotate');
-  } else {
-    sidenav.classList.remove('sidebar-menu-open-anim');
-    sidenavMenuToggleIcon.classList.remove('sidebar-menu-close-icon-rotate');
+function closeSideMenuOnResize() {
+  if (sideMenu) {
+    if (window.innerWidth < 991) {
+      sideMenu.classList.add("side-menu-close");
+      sideMenuTrigger.classList.add("rotate");
+      //document.documentElement.style.setProperty("--side-menu-width", "3em");
+    } else {
+      sideMenu.classList.remove("side-menu-close");
+      sideMenuTrigger.classList.remove("rotate");
+      //document.documentElement.style.setProperty("--side-menu-width", "24em");
+    }
   }
 }
 
-var tableDropdowTrigger = document.querySelector(
-  '.table-dropdown-custom-trigger'
-);
-//TESTING NEW DROPDOWN
-// tableDropdowTrigger.addEventListener('focus', function () {
-//   var el = document.createElement('ul');
-//   el.innerHTML = `
-//   <li class="table-dropdown-custom-list-item">
-//     <a href="#" class="table-dropdown-custom-list-item-link">Open profile</a>
-//   </li>
+window.addEventListener("resize", closeSideMenuOnResize);
 
-//   <li class="table-dropdown-custom-list-item">
-//       <a href="#" class="table-dropdown-custom-list-item-link">Delete member</a>
-//   </li>
-//   `;
-//   el.classList.add('test');
-//   document.body.appendChild(el);
-// });
-
-window.addEventListener('resize', closeMenuOnSmall);
-
-closeMenuOnSmall();
+closeSideMenuOnResize();
